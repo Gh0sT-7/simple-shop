bf3703acb6d634ee1ab311a65344af89d24a45a1
James Mills <james.mills@lontra.co.uk>
2024-03-21 13:00:52 +0000

    Making the app component even leaner.

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,14 @@
-import { useState } from 'react';
-
 import Header from './components/Header/Header.jsx';
 import Shop from './components/Shop/Shop.jsx';
 import Product from './components/Shop/Product.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
-import { CartContext } from './store/shopping-cart-context.jsx';
+import CartContextProvider from './store/shopping-cart-context.jsx';
 
 
 function App() {
-    const [shoppingCart, setShoppingCart] = useState({
-        items: [],
-    });
-
-    /**
-     * Updates the shopping cart by adding an item with the given id, or incrementing the quantity if the item already exists.
-     *
-     * @param {type} id - The id of the item to be added to the cart
-     * @return {type} An object representing the updated shopping cart
-     */
-    function handleAddItemToCart(id) {
-        setShoppingCart((prevShoppingCart) => {
-            const updatedItems = [...prevShoppingCart.items];
-
-            const existingCartItemIndex = updatedItems.findIndex(
-                (cartItem) => cartItem.id === id
-            );
-            const existingCartItem = updatedItems[existingCartItemIndex];
-
-            if (existingCartItem) {
-                const updatedItem = {
-                    ...existingCartItem,
-                    quantity: existingCartItem.quantity + 1,
-                };
-                updatedItems[existingCartItemIndex] = updatedItem;
-            } else {
-                const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-                updatedItems.push({
-                    id: id,
-                    name: product.title,
-                    price: product.price,
-                    quantity: 1,
-                });
-            }
-
-            return {
-                items: updatedItems,
-            };
-        });
-    }
-
-    /**
-     * Updates the quantity of a cart item.
-     *
-     * @param {string} productId - The ID of the product to update.
-     * @param {number} amount - The amount to update the quantity by.
-     * @return {void} This function does not return a value.
-     */
-    function handleUpdateCartItemQuantity(productId, amount) {
-        setShoppingCart((prevShoppingCart) => {
-            const updatedItems = [...prevShoppingCart.items];
-            const updatedItemIndex = updatedItems.findIndex(
-                (item) => item.id === productId
-            );
-
-            const updatedItem = {
-                ...updatedItems[updatedItemIndex],
-            };
-
-            updatedItem.quantity += amount;
-
-            if (updatedItem.quantity <= 0) {
-                updatedItems.splice(updatedItemIndex, 1);
-            } else {
-                updatedItems[updatedItemIndex] = updatedItem;
-            }
-
-            return {
-                items: updatedItems,
-            };
-        });
-    }
-
-
-    const contextValue = {
-        items: shoppingCart.items,
-        addItemToCart: handleAddItemToCart,
-        updateItemQuantity: handleUpdateCartItemQuantity,
-    };
 
     return (
-        <CartContext.Provider value={contextValue}>
+        <CartContextProvider>
             <Header />
 
             <Shop>
@@ -99,7 +18,7 @@ function App() {
                     </li>
                 ))}
             </Shop>
-        </CartContext.Provider>
+        </CartContextProvider>
     );
 }
 
--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -1,7 +1,96 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { DUMMY_PRODUCTS } from "../dummy-products";
 
 export const CartContext = createContext({
     items: [],
     addItemToCart: () => {},
     updateItemQuantity: () => {}
 });
+
+export default function CartContextProvider({ children }) {
+    const [shoppingCart, setShoppingCart] = useState({
+        items: [],
+    });
+
+    /**
+     * Updates the shopping cart by adding an item with the given id, or incrementing the quantity if the item already exists.
+     *
+     * @param {type} id - The id of the item to be added to the cart
+     * @return {type} An object representing the updated shopping cart
+     */
+    function handleAddItemToCart(id) {
+        setShoppingCart((prevShoppingCart) => {
+            const updatedItems = [...prevShoppingCart.items];
+
+            const existingCartItemIndex = updatedItems.findIndex(
+                (cartItem) => cartItem.id === id
+            );
+            const existingCartItem = updatedItems[existingCartItemIndex];
+
+            if (existingCartItem) {
+                const updatedItem = {
+                    ...existingCartItem,
+                    quantity: existingCartItem.quantity + 1,
+                };
+                updatedItems[existingCartItemIndex] = updatedItem;
+            } else {
+                const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+                updatedItems.push({
+                    id: id,
+                    name: product.title,
+                    price: product.price,
+                    quantity: 1,
+                });
+            }
+
+            return {
+                items: updatedItems,
+            };
+        });
+    }
+
+    /**
+     * Updates the quantity of a cart item.
+     *
+     * @param {string} productId - The ID of the product to update.
+     * @param {number} amount - The amount to update the quantity by.
+     * @return {void} This function does not return a value.
+     */
+    function handleUpdateCartItemQuantity(productId, amount) {
+        setShoppingCart((prevShoppingCart) => {
+            const updatedItems = [...prevShoppingCart.items];
+            const updatedItemIndex = updatedItems.findIndex(
+                (item) => item.id === productId
+            );
+
+            const updatedItem = {
+                ...updatedItems[updatedItemIndex],
+            };
+
+            updatedItem.quantity += amount;
+
+            if (updatedItem.quantity <= 0) {
+                updatedItems.splice(updatedItemIndex, 1);
+            } else {
+                updatedItems[updatedItemIndex] = updatedItem;
+            }
+
+            return {
+                items: updatedItems,
+            };
+        });
+    }
+
+
+    const contextValue = {
+        items: shoppingCart.items,
+        addItemToCart: handleAddItemToCart,
+        updateItemQuantity: handleUpdateCartItemQuantity,
+    };
+
+    return (
+        <CartContext.Provider value={contextValue}>
+            {children}
+        </CartContext.Provider>
+    );
+}
